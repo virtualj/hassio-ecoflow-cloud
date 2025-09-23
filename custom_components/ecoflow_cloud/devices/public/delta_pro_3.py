1895be8ffa890fcc106de13d49290d0ca0a94694
virtualj <virtualj82@gmail.com>
2025-09-24 00:50:46 +0200

    Update delta_pro_3.py
    added sensors and sliders

--- custom_components/ecoflow_cloud/devices/public/delta_pro_3.py
+++ custom_components/ecoflow_cloud/devices/public/delta_pro_3.py
@@ -5,12 +5,16 @@ from ...entities import (
     BaseSensorEntity,
     BaseSwitchEntity,
 )
-from ...number import ChargingPowerEntity
+from ...number import (
+    ChargingPowerEntity,
+    MaxBatteryLevelEntity,
+)
 from ...sensor import (
     CapacitySensorEntity,
     InWattsSensorEntity,
     LevelSensorEntity,
     OutWattsSensorEntity,
+    CelsiusSensorEntity,
 )
 from .. import BaseDevice, const
 
@@ -18,17 +22,17 @@ from .. import BaseDevice, const
 class DeltaPro3(BaseDevice):
     def sensors(self, client: EcoflowApiClient) -> list[BaseSensorEntity]:
         return [
-            LevelSensorEntity(
-                client, self, "bmsBattSoc", const.MAIN_BATTERY_LEVEL
-            ).attr("bmsDesignCap", const.ATTR_DESIGN_CAPACITY, 0),
-            CapacitySensorEntity(
-                client, self, "bmsDesignCap", const.MAIN_DESIGN_CAPACITY, False
-            ),
+            LevelSensorEntity(client, self, "bmsBattSoc", const.MAIN_BATTERY_LEVEL).attr("bmsDesignCap", const.ATTR_DESIGN_CAPACITY, 0),
+            CapacitySensorEntity(client, self, "bmsDesignCap", const.MAIN_DESIGN_CAPACITY, False),
             LevelSensorEntity(client, self, "cmsBattSoc", const.COMBINED_BATTERY_LEVEL),
             InWattsSensorEntity(client, self, "powInSumW", const.TOTAL_IN_POWER),
             OutWattsSensorEntity(client, self, "powOutSumW", const.TOTAL_OUT_POWER),
             InWattsSensorEntity(client, self, "powGetAcIn", const.AC_IN_POWER),
-        ]
+            InWattsSensorEntity(client, self, "powGetPvH", const.PV_HV_IN_POWER),
+            InWattsSensorEntity(client, self, "powGetPvL", const.PV_LV_IN_POWER),
+            CelsiusSensorEntity(client, self, "bmsMinCellTemp", const.MIN_CELL_TEMP),
+            CelsiusSensorEntity(client, self, "bmsMaxCellTemp", const.MAX_CELL_TEMP),
+            ]
 
     def numbers(self, client: EcoflowApiClient) -> list[BaseNumberEntity]:
         return [
@@ -47,6 +51,40 @@ class DeltaPro3(BaseDevice):
                     "cmdFunc": 254,
                     "dest": 2,
                     "params": {"cfgPlugInInfoAcInChgPowMax": value},
+                },
+            ),
+            MaxBatteryLevelEntity(
+                client,
+                self,
+                "cfgMaxChgSoc",
+                const.MAX_CHARGE_LEVEL,
+                50,
+                100,
+                lambda value: {
+                    "sn": self.device_info.sn,
+                    "cmdId": 17,
+                    "dirDest": 1,
+                    "dirSrc": 1,
+                    "cmdFunc": 254,
+                    "dest": 2,
+                    "params": {"cfgMaxChgSoc": value},
+                },
+            ),
+            MaxBatteryLevelEntity(
+                client,
+                self,
+                "energyBackupStartSocc",
+                const.BACKUP_RESERVE_LEVEL,
+                5,
+                100,
+                lambda value: {
+                    "sn": self.device_info.sn,
+                    "cmdId": 17,
+                    "dirDest": 1,
+                    "dirSrc": 1,
+                    "cmdFunc": 254,
+                    "dest": 2,
+                    "params": {"cfgEnergyBackup": {"energyBackupStartSoc": value, "energyBackupEn": True}},
                 },
             ),
         ]
